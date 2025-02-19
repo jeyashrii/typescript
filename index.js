7dcce15a769584563c709603325dd5b4e8d6be53
jeyashrii <jeyashri30@gmail.com>
2025-02-19 23:09:38 +0530

    ts

---
+++ index.js
@@ -0,0 +1,6 @@
+var myname = "jegtry";
+console.log(myname);
+function add1(num) {
+    return num + 1;
+}
+console.log(add1(3));
---
+++ index.ts
@@ -0,0 +1,6 @@
+let myname: string = "jegtry";
+console.log(myname);
+function add1(num: number) {
+  return num + 1;
+}
+console.log(add1(3));
